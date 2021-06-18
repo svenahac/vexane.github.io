88c169912dd23c20e0014911c53a5bd7c3e0e1ef
vexane <ahacsh@gmail.com>
2021-06-18 11:50:18 +0200

    update

--- js/script.js
+++ js/script.js
@@ -15,3 +15,33 @@ console.log(`
 ⠄⠄⠄⠄⠄⠄⠄⠉⠻⣿⣿⣾⣦⡙⠻⣷⣾⣿⠃⠿⠋⠁⠄⠄⠄⠄⠄⢀⣠⣴
 ⣿⣿⣿⣶⣶⣮⣥⣒⠲⢮⣝⡿⣿⣿⡆⣿⡿⠃⠄⠄⠄⠄⠄⠄⠄⣠⣴⣿⣿⣿
 `)
+/*
+var btnContainer = document.getElementById("navJS");
+
+var btns = btnContainer.getElementsByClassName("nav-link");
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+*/
+
+var btnContainer = document.getElementById("navJS");
+var btns = btnContainer.getElementsByClassName("nav-link");
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += " active";
+  });
+}
